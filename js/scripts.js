d8b139d3f00ff810cb3d771285cc098b7ebe4321
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-02 20:02:57 -0600

    add five form questions and beginning of JavaScript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-$(document).ready(function(event) {
-  $("form.choose-animal").submit(function(event) {
+// Back-End (business logic)
+
+
+
+// Front-End (user-interface logic)
+$(document).ready(function() {
+  $("form#lang-genie").submit(function(event) {
     event.preventDefault();
 
-    var chosenAnimal = $("#choose").val();
+    //var outputOne = parseInt($("#favAnimal").val());
     
-    if (chosenAnimal === "elephant") {
+    //alert("outputOne");
+    //var output = parseInt($("#question1").val());
+    
+    if (output < 33) {
       $("#elephant").show(); 
       $("#duck").hide();
       $("#capybara").hide();
-    } else if (chosenAnimal === "duck") {
+    } else if (output >= 33 && output <= 100) {
       $("#elephant").hide();
       $("#duck").show();
       $("#capybara").hide();
