80d4fd6d59cb7cd4c9a4b0b3457c06306424767b
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-02 21:36:08 -0600

    add working javascript and jquery code for branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
 // Back-End (business logic)
 
 
-
 // Front-End (user-interface logic)
-$(document).ready(function() {
-  $("form#lang-genie").submit(function(event) {
+$(document).ready(function () {
+  $("form#lang-genie").submit(function () {
     event.preventDefault();
+    var favAnimal = parseInt($("#favAnimal").val());
+    console.log(favAnimal); //for debugging
+    var wouldYouRather = parseInt($("input:radio[name=iWouldRather]:checked").val());
+    console.log(wouldYouRather); //for debugging
+    var birthMonth = parseInt($("#birthMonth").val());
+    console.log(birthMonth); //for debugging
 
-    //var outputOne = parseInt($("#favAnimal").val());
-    
-    //alert("outputOne");
-    //var output = parseInt($("#question1").val());
-    
-    if (output < 33) {
-      $("#elephant").show(); 
-      $("#duck").hide();
-      $("#capybara").hide();
-    } else if (output >= 33 && output <= 100) {
-      $("#elephant").hide();
-      $("#duck").show();
-      $("#capybara").hide();
+    var output = favAnimal + wouldYouRather + birthMonth;
+    console.log(output);
+
+    if (output < 12) {
+      $("#javascript").show(); 
+      $("#ruby").hide();
+      $("#python").hide();
+    } else if (output >= 12 && output <= 20) {
+      $("#javascript").hide();
+      $("#ruby").show();
+      $("#python").hide();
     } else { 
-      $("#elephant").hide();
-      $("#duck").hide();
-      $("#capybara").show();
+      $("#javascript").hide();
+      $("#ruby").hide();
+      $("#python").show();
     }
-  
+
   })
 })
